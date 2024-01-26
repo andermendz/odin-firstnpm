d3a859fb24c0514d4fe08383723ad8f0cc86544c
andermendz <andersonmendz19@gmail.com>
2024-01-26 18:17:29 -0500

    basic bundling

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import myName from './myName';
 import numbers from './numbers';
 import { functionOne } from './counter';
+import './style.css';
+import logo from './logo.png';
+
 
 function component() {
     const element = document.createElement('div');
     const elementTwo = document.createElement('div')
     
+    const icon = new Image();
+    icon.src = logo;
     
     element.innerHTML = myName('Anderson')
     
     elementTwo.innerHTML = numbers(20);
 
     element.appendChild(elementTwo)
+    element.classList.add('hello');
 
-
+    
     element.appendChild(functionOne(5))
+    //element.appendChild(icon);
 
     return element;
    
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },  
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack tutorial',
+      filename: 'index.html',
+      inject: 'body',
+      scriptLoading: 'defer',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
+      },
+      {
+
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+
+      },
+  
+    ],
   },
 };
